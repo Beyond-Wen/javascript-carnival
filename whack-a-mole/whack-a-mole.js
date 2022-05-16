2e7dd5be2c909aad98f27b4f7051534dbbfb1324
Beyond Wen <allstar_beyond@hotmail.com>
2022-05-16 20:31:21 +1200

    create mole js

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,32 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+let cellsArray = document.getElementsByTagName('td')
+
+//create mole image
+let molePic = document.createElement('img')
+molePic.src = 'mole.PNG'
+//create audio
+let boing = new Audio()
+boing.src = ' whack-audio.wav'
+
+//function to make mole appear in random cell
+function moleAppear() {
+  let randomIndex = Math.floor(Math.random() * 25)
+  let randomCell = cellsArray[randomIndex]
+  randomCell.appendChild(molePic)
+}
+moleAppear()
+
+//clicking on mole
+
+molePic.onclick = whackedMole
+
+function whackedMole() {
+  molePic.remove(molePic)
+  moleAppear()
+  boing.play()
+}
